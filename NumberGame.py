666902e391dee999b6f3f270a9c9551ba5f69a98
CynicalIndian <42709261+CynicalIndian@users.noreply.github.com>
2018-08-28 19:43:55 +0530

    added some comment statements
    nothing relevant

--- NumberGame.py
+++ NumberGame.py
@@ -1,6 +1,6 @@
-userInput = 0
-userChoice = 'u'
-contu = True
+userInput = 0 #userInput value has been set to 0 by default
+userChoice = 'u'  #userChoice value has been set to undefined or u
+contu = True    #contu value by default is set to true
 while contu == True:
     print ('Welcome to number games.')
     print ('Enter a number, and if it is even, You win.')
